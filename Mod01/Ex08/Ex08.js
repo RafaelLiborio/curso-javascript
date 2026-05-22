9b375f260caa8551e62b21387d955637c6b03424
RafaelLiborio <Rafaelliborio57@gmail.com>
2026-05-22 07:59:49 -0300

    Curso de javascript

---
+++ Mod01/Ex08/Ex08.js
@@ -0,0 +1,9 @@
+var vel = 150
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel >= 50){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}else{
+    console.log(`Você está dentro dos padrões de direção`)
+}
+ 
