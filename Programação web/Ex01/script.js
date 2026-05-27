42637b0f93dc597d8603ebaeb90cf70bde94b11c
RafaelLiborio <Rafaelliborio57@gmail.com>
2026-05-27 14:43:15 -0300

    commit mudança de cores

--- Programação web/Ex01/script.js
+++ Programação web/Ex01/script.js
@@ -1,5 +1,20 @@
 function eventoClick(){
     //alert("Acionou um Evento")
     document.body.style.background = "yellow";
-    document.body.style.transition = '10s'
+}
+
+function eventoDblClick(){
+    document.body.style.background = "green";
+}
+
+function viraVermelho(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "blue"
+    div.style.color = "white";
+}
+
+function viraAzul() {
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "salmon"
+    div.style.color = "black"
 }
